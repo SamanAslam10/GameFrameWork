268b27485fe60043949ee3984831686e05af9b08
Saman Aslam <samanaslam835@gmail.com>
2025-12-21 02:13:57 +0500

    Add Jump Movements + gravity (no ground yet)

---
+++ Movements/JumpMovement.cs
@@ -0,0 +1,22 @@
+﻿using EZInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantsVsZombies.Movements
+{
+    internal class JumpMovement : IMovement
+    {
+        private float jumpPower = 20f;
+        public void Move(GameObject obj, GameTime gameTime)
+        {
+            if (Keyboard.IsKeyPressed(Key.Space) && Keyboard.IsKeyPressed(Key.Control))
+            {
+                obj.Velocity = new PointF(obj.Velocity.X, -jumpPower);
+                obj.HasPhysics = true;
+            }
+        }
+    }
+}
