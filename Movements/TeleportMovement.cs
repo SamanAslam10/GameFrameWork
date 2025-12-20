dc9ec651281e18c10a1a63aefa930ee02c5fbc88
Saman Aslam <samanaslam835@gmail.com>
2025-12-21 03:27:07 +0500

    Add Random Teleport Movement (shift key)

---
+++ Movements/TeleportMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EZInput;
+
+namespace PlantsVsZombies.Movements
+{
+    internal class Teleport : IMovement
+    {
+        Random random = new Random();
+        private float width;
+        private float height; 
+        public Teleport(float width , float height) 
+        {
+            this.width = width;
+            this.height = height;
+        }   
+        public void Move(GameObject obj , GameTime gameTime) 
+        {
+            if (Keyboard.IsKeyPressed(Key.Shift)) 
+            {
+                obj.Position = RandomPosition(obj.Size);
+            }
+        }
+        private PointF RandomPosition(SizeF size) 
+        {
+            float x = random.Next(0, (int)(width - size.Width));
+            float y = random.Next(0, (int)(height - size.Height));
+            return new PointF(x, y);
+        }
+    }
+}
