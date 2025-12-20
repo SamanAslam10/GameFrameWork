5199197cb3db9f8b5f779c3d0f856f1a3f590f41
Saman Aslam <samanaslam835@gmail.com>
2025-12-21 02:12:25 +0500

    Add Diagonal Movement (Space + Ctrl)

---
+++ Movements/DiagonalJumpMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EZInput;
+
+namespace PlantsVsZombies.Movements
+{
+    internal class DiagonalJumpMovement : IMovement
+    {
+        private int jump = 20;
+        public void Move(GameObject obj , GameTime gameTime) 
+        {
+            if (Keyboard.IsKeyPressed(Key.Space) && Keyboard.IsKeyPressed(Key.RightArrow)) 
+            {
+                obj.Position = new PointF(obj.Position.X + jump , obj.Position.Y - jump);
+            }
+            if (Keyboard.IsKeyPressed(Key.Space) && Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                obj.Position = new PointF(obj.Position.X-jump, obj.Position.Y - jump);
+            }
+        }
+    }
+}
