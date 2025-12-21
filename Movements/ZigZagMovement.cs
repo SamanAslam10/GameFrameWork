766aa9e5578337448ab434ce0f7da2666a2a1eb8
Saman Aslam <samanaslam835@gmail.com>
2025-12-21 10:55:46 +0500

    Add ZigZag Movement (Space + right/left)

---
+++ Movements/ZigZagMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EZInput;
+
+namespace GameFrameWork.Movements
+{
+    internal class ZigZagMovement : IMovement
+    {
+        private int move = 20;
+        public void Move(GameObject obj , GameTime gameTime) 
+        {
+            if (Keyboard.IsKeyPressed(Key.Space) && Keyboard.IsKeyPressed(Key.RightArrow)) 
+            {
+                obj.Position = new PointF(obj.Position.X + move , obj.Position.Y - move);
+            }
+            if (Keyboard.IsKeyPressed(Key.Space) && Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                obj.Position = new PointF(obj.Position.X-move, obj.Position.Y - move);
+            }
+        }
+    }
+}
