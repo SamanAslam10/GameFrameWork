cdedf65a06bc817bf7d9a05026e79e9abf4f86fa
Saman Aslam <samanaslam835@gmail.com>
2025-12-23 03:12:11 +0500

    Add a Sound Manager

---
+++ Systems/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace GameFrameWork
+{
+    internal class SoundManager
+    {
+        private SoundPlayer backGroundSound;
+        private SoundPlayer winSound;
+        private SoundPlayer loseSound;
+        private SoundPlayer damageSound;
+        private SoundPlayer scoreSound;
+        
+        public void BackgroundPlay(string path) 
+        {
+            backGroundSound = new SoundPlayer(path);
+            backGroundSound.PlayLooping();
+        }
+        public void winPlay(string path)
+        {
+            winSound = new SoundPlayer(path);
+            winSound.Play();
+        }
+        public void losePlay(string path)
+        {
+            loseSound = new SoundPlayer(path);
+            loseSound.Play();
+        }
+        public void damagePlay(string path)
+        {
+            damageSound = new SoundPlayer(path);
+            damageSound.Play();
+        }
+        public void scorePlay(string path)
+        {
+            scoreSound = new SoundPlayer(path);
+            scoreSound.Play();
+        }
+    }
+}
