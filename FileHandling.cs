219e284380fd7c3936b0e13a8a360d791f5cd935
Saman Aslam <samanaslam835@gmail.com>
2025-12-28 15:34:18 +0500

    Add File Handling

---
+++ FileHandling.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFrameWork
+{
+    internal class FileHandling
+    {
+        public void Save(int level) 
+        {
+            File.WriteAllText("Progress.txt", level.ToString());
+        }
+        public int Load()
+        {
+            if (!File.Exists("Progress.txt"))
+            {
+                return 1;
+            }
+            else 
+            {
+                return int.Parse(File.ReadAllText("Progress.txt"));
+            }
+        }
+    }
+}
