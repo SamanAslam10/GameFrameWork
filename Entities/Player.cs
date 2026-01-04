1900d33056450eec1c1d41d91111e3c036c4c9e4
Saman Aslam <samanaslam835@gmail.com>
2026-01-04 13:05:59 +0500

    Add the Cattai plant bullet logic

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -85,6 +85,17 @@ namespace GameFrameWork
                     IsRigidBody = false
                 });
             }
+            else if (PlantType == "cattai")
+            {
+                GameRef.AddObject(new Bullet
+                {
+                    Sprite = new StaticSprite(Resources.spikeBullet_),
+                    Size = new SizeF(60, 60),
+                    Position = new PointF(Position.X + Size.Width, Position.Y),
+                    Movement = new MoveRightMovement(10f),
+                    IsRigidBody = false
+                });
+            }
         }
         /// Draw uses base implementation; override if player needs custom visuals.
 
